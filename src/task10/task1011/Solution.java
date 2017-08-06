0f8dd728e46216b2333a2bda9a3b14655fb64ff1
mitrex4444 <44heroh/2mail.ru>
2017-08-06 12:15:30 +0300

    task10.task1008

---
+++ src/task10/task1011/Solution.java
@@ -0,0 +1,16 @@
+package task10.task1011;
+
+public class Solution {
+    public static void main(String[] args) {
+        String s = "Я не хочу изучать Java, я хочу большую зарплату";
+
+        //напишите тут ваш код
+//        char[] chArray = s.toCharArray();
+        /*for(char c : chArray){
+            System.out.println(s.substring(1));
+        }*/
+        for(int i = 0; i < 40; i++){
+            System.out.println(s.substring(i));
+        }
+    }
+}
