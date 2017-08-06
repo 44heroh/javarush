e2eb6736663b91623dfc13c069b407b1dc0f1a3e
mitrex4444 <44heroh/2mail.ru>
2017-08-06 11:46:26 +0300

    Initial commit

---
+++ src/task10/task1008/Solution.java
@@ -0,0 +1,10 @@
+package task10.task1008;
+
+public class Solution {
+    public static void main(String[] args) {
+        int a = 50;
+        int b = 17;
+        double d = a / b;
+        System.out.println(d);
+    }
+}
