59b10dd658e4c222792024910695527bbc58c7ec
mitrex4444 <44heroh/2mail.ru>
2017-08-23 17:43:07 +0300

    task11.task1113

---
+++ src/task11/task1113/Solution.java
@@ -0,0 +1,25 @@
+package task11.task1113;
+
+/**
+ * Created by User on 23.08.2017.
+ */
+public class Solution {
+    public static void main(String[] args) {
+    }
+
+    public class Fish {
+
+    }
+
+    public class Animal extends Fish {
+
+    }
+
+    public class Ape extends Animal{
+
+    }
+
+    public class Human extends Ape {
+
+    }
+}
