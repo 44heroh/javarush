765cc9c1109fb84181dfd810124fcb4381949e33
mitrex4444 <44heroh/2mail.ru>
2017-08-23 19:10:57 +0300

    task11.task1121

---
+++ src/task11/task1121/Solution.java
@@ -0,0 +1,29 @@
+package task11.task1121;
+
+/**
+ * Created by User on 23.08.2017.
+ */
+public class Solution {
+    public static void main(String[] args) {
+    }
+
+    public class Pet {
+
+    }
+
+    public class Cat extends Pet {
+
+    }
+
+    public class Dog extends Pet {
+
+    }
+
+    public class House {
+
+    }
+
+    public class Airplane {
+
+    }
+}
