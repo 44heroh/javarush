b4fce2e65034bebf27ee17ba7ebfec0fd98b88a9
mitrex4444 <44heroh/2mail.ru>
2017-08-23 19:03:55 +0300

    task11.task1117

---
+++ src/task11/task1117/Solution.java
@@ -0,0 +1,29 @@
+package task11.task1117;
+
+/**
+ * Created by User on 23.08.2017.
+ */
+public class Solution {
+    public static void main(String[] args) {
+    }
+
+    public class Carnivore extends Animal {
+
+    }
+
+    public class Cow extends Animal {
+
+    }
+
+    public class Dog extends Carnivore {
+
+    }
+
+    public class Pig extends Animal {
+
+    }
+
+    public class Animal {
+
+    }
+}
