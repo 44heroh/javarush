aaf4addd9f1bc0bc3f70c7bc571667a9410b8055
mitrex4444 <44heroh/2mail.ru>
2017-08-23 17:39:29 +0300

    task11.task1111

---
+++ src/task11/task1111/Solution.java
@@ -0,0 +1,19 @@
+package task11.task1111;
+
+/**
+ * Created by User on 23.08.2017.
+ */
+public class Solution {
+    public static void main(String[] args) {
+    }
+
+    //Адам
+    public class Adam {
+
+    }
+
+    //Ева
+    public class Eve extends Adam {
+
+    }
+}
