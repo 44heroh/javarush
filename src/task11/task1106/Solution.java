8599ade12a966ff739225da4ea6646ec5e2664eb
mitrex4444 <44heroh/2mail.ru>
2017-08-23 15:12:20 +0300

    task11.task1106

---
+++ src/task11/task1106/Solution.java
@@ -0,0 +1,24 @@
+package task11.task1106;
+
+/**
+ * Created by User on 23.08.2017.
+ */
+public class Solution {
+    public static void main(String[] args) {
+    }
+
+    public class Cat {
+        private String name;
+        private int age;
+        private int weight;
+
+        public Cat() {
+        }
+
+        public Cat(String name, int age, int weight) {
+            this.name = name;
+            this.age = age;
+            this.weight = weight;
+        }
+    }
+}
