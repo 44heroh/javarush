a095f35ef9344870afbe097f9051553f486ce8ea
mitrex4444 <44heroh/2mail.ru>
2017-08-23 17:41:05 +0300

    task11.task1112

---
+++ src/task11/task1112/Solution.java
@@ -0,0 +1,17 @@
+package task11.task1112;
+
+/**
+ * Created by User on 23.08.2017.
+ */
+public class Solution {
+    public static void main(String[] args) {
+    }
+
+    public class AppleIPhone {
+
+    }
+
+    public class SamsungGalaxyS2 extends AppleIPhone {
+
+    }
+}
