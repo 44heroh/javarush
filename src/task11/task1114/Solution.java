c15158842d72e40af3613bae910a8e3010327f0a
mitrex4444 <44heroh/2mail.ru>
2017-08-23 18:14:07 +0300

    task11.task1114

---
+++ src/task11/task1114/Solution.java
@@ -0,0 +1,21 @@
+package task11.task1114;
+
+/**
+ * Created by User on 23.08.2017.
+ */
+public class Solution {
+    public static void main(String[] args) {
+    }
+
+    public class Judaism {
+
+    }
+
+    public class Christianity extends Judaism {
+
+    }
+
+    public class Islam extends Christianity {
+
+    }
+}
