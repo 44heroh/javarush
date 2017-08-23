7b001790880f15f3f4d17d5fcdf2341e8a93c8d5
mitrex4444 <44heroh/2mail.ru>
2017-08-23 15:20:38 +0300

    task11.task1108

---
+++ src/task11/task1108/Solution.java
@@ -0,0 +1,37 @@
+package task11.task1108;
+
+/**
+ * Created by User on 23.08.2017.
+ */
+public class Solution {
+    public static void main(String[] args) {
+    }
+
+    public class Cat {
+        private String name;
+        private int age;
+        private int weight;
+
+        public Cat(String name, int age, int weight) {
+            this.name = name;
+            this.age = age;
+            this.weight = weight;
+        }
+
+        public String getName() {
+            return name;
+        }
+
+        private void setName(String name) {
+            this.name = name;
+        }
+
+        public int getAge() {
+            return age;
+        }
+
+        private void setAge(int age) {
+            this.age = age;
+        }
+    }
+}
