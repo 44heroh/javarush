ec48bad72aa8a2f8a3b8dce9a979f72e6f3fcd90
mitrex4444 <44heroh/2mail.ru>
2017-08-23 19:07:27 +0300

    task11.task1119

---
+++ src/task11/task1119/Solution.java
@@ -0,0 +1,25 @@
+package task11.task1119;
+
+/**
+ * Created by User on 23.08.2017.
+ */
+public class Solution {
+    public static void main(String[] args) {
+    }
+
+    public class House {
+
+    }
+
+    public class Cat {
+
+    }
+
+    public class Car {
+
+    }
+
+    public class Dog {
+
+    }
+}
