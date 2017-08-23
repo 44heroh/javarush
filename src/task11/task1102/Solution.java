b0eb6ab3bd5505193f975d056936a34210671d00
mitrex4444 <44heroh/2mail.ru>
2017-08-23 14:54:04 +0300

    task11.task1102

---
+++ src/task11/task1102/Solution.java
@@ -0,0 +1,21 @@
+package task11.task1102;
+
+/**
+ * Created by User on 23.08.2017.
+ */
+public class Solution {
+    public static void main(String[] args) {
+    }
+
+    public class Pet {
+
+    }
+
+    public class Cat extends Pet {
+
+    }
+
+    public class Dog extends Pet {
+
+    }
+}
