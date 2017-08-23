15b48983bccdb18f9c08f72a24b82c07480a5e56
mitrex4444 <44heroh/2mail.ru>
2017-08-23 15:05:58 +0300

    task11.task1105

---
+++ src/task11/task1105/Solution.java
@@ -0,0 +1,45 @@
+package task11.task1105;
+
+/**
+ * Created by User on 23.08.2017.
+ */
+public class Solution {
+    public static void main(String[] args) {
+    }
+
+    public class Worker {
+
+    }
+
+    public class Clerk extends Worker{
+
+    }
+
+    public class IT extends Worker{
+
+    }
+
+    public class Programmer extends IT {
+
+    }
+
+    public class ProjectManager extends IT {
+
+    }
+
+    public class CTO extends IT {
+
+    }
+
+    public class OfficeManager extends Clerk {
+
+    }
+
+    public class HR extends Clerk {
+
+    }
+
+    public class Cleaner extends Clerk {
+
+    }
+}
