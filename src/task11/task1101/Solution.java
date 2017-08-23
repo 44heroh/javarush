4f8b4037031db325982f548935f0545d4dbb7d36
mitrex4444 <44heroh/2mail.ru>
2017-08-23 14:50:13 +0300

    task11.task1101

---
+++ src/task11/task1101/Solution.java
@@ -0,0 +1,17 @@
+package task11.task1101;
+
+/**
+ * Created by User on 23.08.2017.
+ */
+public class Solution {
+    public static void main(String[] args) {
+    }
+
+    public class Horse {
+
+    }
+
+    public class Pegas extends Horse {
+
+    }
+}
