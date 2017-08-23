7d72e2b8c3bd78e616eeaf01cb079ae3b74448ba
mitrex4444 <44heroh/2mail.ru>
2017-08-23 14:59:35 +0300

    task11.task1104

---
+++ src/task11/task1104/Solution.java
@@ -0,0 +1,25 @@
+package task11.task1104;
+
+/**
+ * Created by User on 23.08.2017.
+ */
+public class Solution {
+    public static void main(String[] args) {
+    }
+
+    public class Manager extends Employee {
+
+    }
+
+    public class Chief extends Employee {
+
+    }
+
+    public class Employee {
+
+    }
+
+    public class Secretary extends Employee {
+
+    }
+}
