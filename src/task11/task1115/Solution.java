06278d75b969d80d930996f69ec9b9658c76f994
mitrex4444 <44heroh/2mail.ru>
2017-08-23 18:55:36 +0300

    task11.task1115

---
+++ src/task11/task1115/Solution.java
@@ -0,0 +1,25 @@
+package task11.task1115;
+
+/**
+ * Created by User on 23.08.2017.
+ */
+public class Solution {
+    public static void main(String[] args) {
+    }
+
+    public class Schoolboy {
+
+    }
+
+    public class Student extends Schoolboy {
+
+    }
+
+    public class Worker extends Student{
+
+    }
+
+    public class Slave extends Worker{
+
+    }
+}
