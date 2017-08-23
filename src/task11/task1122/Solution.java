d5f31db7f26c421b9215bf12324ddf4081f0b702
mitrex4444 <44heroh/2mail.ru>
2017-08-23 19:13:12 +0300

    task11.task1122

---
+++ src/task11/task1122/Solution.java
@@ -0,0 +1,30 @@
+package task11.task1122;
+
+/**
+ * Created by User on 23.08.2017.
+ */
+public class Solution {
+    public static void main(String[] args) {
+    }
+
+    public class King extends ChessFigure {
+    }
+
+    public class Queen extends ChessFigure {
+    }
+
+    public class Rook extends ChessFigure {
+    }
+
+    public class Knight extends ChessFigure {
+    }
+
+    public class Bishop extends ChessFigure {
+    }
+
+    public class Pawn extends ChessFigure {
+    }
+
+    public class ChessFigure{
+    }
+}
