017a951b41e9d7de414f21f491c2483cb7c66f9b
mitrex4444 <44heroh/2mail.ru>
2017-08-24 09:31:41 +0300

    task7.task0725

---
+++ src/task7/task0725/Solution.java
@@ -0,0 +1,17 @@
+package task7.task0725;
+
+/**
+ * Created by User on 24.08.2017.
+ */
+public class Solution {
+    public final static int A = 5;
+    public final static int B = 2;
+    public final static int C = A * B;
+
+    public static void main(String[] args) {
+    }
+
+    public int getValue() {
+        return C;
+    }
+}
