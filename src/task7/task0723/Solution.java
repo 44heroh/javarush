f5608cf0ef5b5ea2c9d117c450ca5343f9e435a0
mitrex4444 <44heroh/2mail.ru>
2017-08-24 09:19:36 +0300

    task7

---
+++ src/task7/task0723/Solution.java
@@ -0,0 +1,16 @@
+package task7.task0723;
+
+/**
+ * Created by User on 24.08.2017.
+ */
+public class Solution {
+    public static void main(String[] args) {
+        for (int i = 30; i >= 0; i--) {
+            System.out.println(i);
+
+            //напишите тут ваш код
+        }
+
+        System.out.println("Бум!");
+    }
+}
