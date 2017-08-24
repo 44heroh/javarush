11479cd7c44be6b4ae5f9d538b106aea1c0001a2
mitrex4444 <44heroh/2mail.ru>
2017-08-24 09:21:33 +0300

    task7

--- src/task7/task0723/Solution.java
+++ src/task7/task0723/Solution.java
@@ -4,11 +4,12 @@ package task7.task0723;
  * Created by User on 24.08.2017.
  */
 public class Solution {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
         for (int i = 30; i >= 0; i--) {
             System.out.println(i);
 
             //напишите тут ваш код
+            Thread.sleep(100);
         }
 
         System.out.println("Бум!");
