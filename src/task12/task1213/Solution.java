3ead677f5a7a11bb85010f0f1c0b10e694cea929
mitrex4444 <44heroh/2mail.ru>
2017-08-27 13:44:47 +0300

    task12.task1213 - прошла с первой попытки

---
+++ src/task12/task1213/Solution.java
@@ -0,0 +1,18 @@
+package task12.task1213;
+
+/**
+ * Created by User on 27.08.2017.
+ */
+public class Solution {
+    public static void main(String[] args) {
+
+    }
+
+    public abstract static class Pet {
+        public String getName() {
+            return "Я - котенок";
+        }
+
+        public abstract Pet getChild();
+    }
+}
