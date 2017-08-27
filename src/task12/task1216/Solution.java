954ddc24262abcc22033f4dfa370fd114b0051e8
mitrex4444 <44heroh/2mail.ru>
2017-08-27 14:04:21 +0300

    task12.task1216 - прошла с первой попытки

---
+++ src/task12/task1216/Solution.java
@@ -0,0 +1,17 @@
+package task12.task1216;
+
+/**
+ * Created by User on 27.08.2017.
+ */
+public class Solution {
+    public static void main(String[] args) {
+
+    }
+
+    //add an interface here - добавь интерфейс тут
+
+    public interface Fly{
+        void getFly();
+        void setFly();
+    }
+}
