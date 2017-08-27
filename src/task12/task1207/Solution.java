07a841f79e4d898da0846ef4448a79a6dd03959a
mitrex4444 <44heroh/2mail.ru>
2017-08-27 13:11:53 +0300

    task12.task1207 - прошла с первой попытки

---
+++ src/task12/task1207/Solution.java
@@ -0,0 +1,21 @@
+package task12.task1207;
+
+/**
+ * Created by User on 27.08.2017.
+ */
+public class Solution {
+    public static void main(String[] args){
+        int valInt = 2;
+        Integer valInteger = 2;
+        print(valInt);
+        print(valInteger);
+    }
+
+    public static void print(int val){
+
+    }
+
+    public static void print(Integer val){
+
+    }
+}
