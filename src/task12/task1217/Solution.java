c202c8ae6ead4ccbf2e7ea5490b6b37efb2aed67
mitrex4444 <44heroh/2mail.ru>
2017-08-27 14:06:20 +0300

    task12.task1217 - прошла с первой попытки

---
+++ src/task12/task1217/Solution.java
@@ -0,0 +1,22 @@
+package task12.task1217;
+
+/**
+ * Created by User on 27.08.2017.
+ */
+public class Solution {
+    public static void main(String[] args) {
+
+    }
+
+//add interfaces here - добавь интерфейсы тут
+
+    interface Fly{
+        void getFly();
+    }
+    interface Run {
+        void getRun();
+    }
+    interface Swim  {
+        void getSwim();
+    }
+}
