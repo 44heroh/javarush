e3ca24e30b5b8bbaeebf26bb1bac5a804ee9aaa5
mitrex4444 <44heroh/2mail.ru>
2017-08-27 14:36:53 +0300

    task12.task1223 - прошла с первой попытки

---
+++ src/task12/task1223/Solution.java
@@ -0,0 +1,35 @@
+package task12.task1223;
+
+/**
+ * Created by User on 27.08.2017.
+ */
+public class Solution {
+    public static void main(String[] args) {
+        Pet pet = new Cat();
+        pet.setName("Я - пушистик");
+
+        System.out.println(pet.getName());
+    }
+
+    public static class Pet {
+        protected String name;
+
+        public Pet() {
+        }
+
+        public final String getName() {
+            return name;
+        }
+
+        public void setName(String name) {
+            this.name = name;
+        }
+
+    }
+
+    public static class Cat extends Pet {
+        public void setName(String name) {
+            this.name = "";
+        }
+    }
+}
