e1375cc1f6981848a37a63e2e6553c4054b8c0d6
mitrex4444 <44heroh/2mail.ru>
2017-08-27 13:20:12 +0300

    task12.task1208 - прошла с первой попытки

---
+++ src/task12/task1208/Solution.java
@@ -0,0 +1,36 @@
+package task12.task1208;
+
+/**
+ * Created by User on 27.08.2017.
+ */
+public class Solution {
+    public static void main(String[] args) {
+        int valInt = 2;
+        Integer valInteger = 2;
+        String valString = "2";
+        char valChar = '2';
+        boolean valBoolean = true;
+
+        print(valInt);
+        print(valInteger);
+        print(valString);
+        print(valChar);
+        print(valBoolean);
+    }
+
+    public static void print(String val){
+
+    }
+    public static void print(int val){
+
+    }
+    public static void print(Integer val){
+
+    }
+    public static void print(char val){
+
+    }
+    public static void print(Boolean val){
+
+    }
+}
