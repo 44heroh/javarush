ecb2c1f6cc816ca7f6ee36f44eb914152c007795
mitrex4444 <44heroh/2mail.ru>
2017-08-27 12:41:28 +0300

    task10.task1019 - прошла с первой попытки

---
+++ src/task10/task1019/Solution.java
@@ -0,0 +1,27 @@
+package task10.task1019;
+
+import java.io.*;
+import java.util.HashMap;
+import java.util.Map;
+
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        HashMap<String, Integer> map = new HashMap<>();
+
+        int id = 0;
+        String name = "";
+        while (true){
+            try{
+                id = Integer.parseInt(reader.readLine());
+                name = reader.readLine();
+                map.put(name, id);
+            }catch (Exception e){
+                break;
+            }
+        }
+        for(Map.Entry<String, Integer> pair : map.entrySet()){
+            System.out.println(pair.getValue() + " " + pair.getKey());
+        }
+    }
+}
