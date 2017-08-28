b104b6648bb71a79bf9da031b1ba8873144c22f1
mitrex4444 <44heroh/2mail.ru>
2017-08-28 14:47:48 +0300

    task13.task1301 - прошла с первой попытки

---
+++ src/task13/task1301/Solution.java
@@ -0,0 +1,45 @@
+package task13.task1301;
+
+/**
+ * Created by User on 28.08.2017.
+ */
+public class Solution {
+    public static void main(String[] args) throws Exception {
+    }
+
+    public interface Drink {
+        void askMore(String message);
+
+        void sayThankYou();
+
+        boolean isReadyToGoHome();
+    }
+
+    public interface Alcohol extends Drink {
+        boolean READY_TO_GO_HOME = false;
+
+        void sleepOnTheFloor();
+    }
+
+    public static class Beer implements Alcohol {
+        @Override
+        public void sleepOnTheFloor(){
+
+        }
+
+        @Override
+        public void askMore(String message) {
+
+        }
+
+        @Override
+        public void sayThankYou() {
+
+        }
+
+        @Override
+        public boolean isReadyToGoHome() {
+            return READY_TO_GO_HOME;
+        }
+    }
+}
