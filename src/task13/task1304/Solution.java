1d671a00e88fc28c5de6fd11f83d0f68482906ae
mitrex4444 <44heroh/2mail.ru>
2017-08-28 14:57:43 +0300

    task13.task1304 - прошла с первой попытки

---
+++ src/task13/task1304/Solution.java
@@ -0,0 +1,28 @@
+package task13.task1304;
+
+/**
+ * Created by User on 28.08.2017.
+ */
+public class Solution {
+    public static void main(String[] args) throws Exception {
+    }
+
+    interface Selectable {
+        void onSelect();
+    }
+
+    interface Updatable {
+        void refresh();
+    }
+
+    //напишите тут ваш класс
+    public static class Screen implements Selectable, Updatable{
+        public void onSelect(){
+
+        }
+
+        public void refresh(){
+
+        }
+    }
+}
