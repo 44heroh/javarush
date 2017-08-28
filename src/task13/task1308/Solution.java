435fb92ae11d3b598bb9f5486b6bd3c2181d57ce
mitrex4444 <44heroh/2mail.ru>
2017-08-28 15:09:53 +0300

    task13.task1308 - прошла с первой попытки

---
+++ src/task13/task1308/Solution.java
@@ -0,0 +1,17 @@
+package task13.task1308;
+
+/**
+ * Created by User on 28.08.2017.
+ */
+public class Solution {
+    public static void main(String[] args) throws Exception {
+    }
+
+    public interface Person{
+        boolean isAlive();
+    }
+
+    public interface Presentable extends Person{
+
+    }
+}
