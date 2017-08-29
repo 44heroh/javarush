19bc44c0db464bc11e055ce778c8847284f8d0ac
mitrex4444 <44heroh/2mail.ru>
2017-08-29 23:17:04 +0300

    task13.task1324 - прошла с первой попытки

---
+++ src/task13/task1324/Solution.java
@@ -0,0 +1,20 @@
+package task13.task1324;
+
+import java.awt.*;
+
+public class Solution {
+    public static void main(String[] args) throws Exception {
+    }
+
+    public interface Animal {
+        Color getColor();
+
+        Integer getAge();
+    }
+
+    public abstract static class Fox implements Animal{
+        public String getName() {
+            return "Fox";
+        }
+    }
+}
