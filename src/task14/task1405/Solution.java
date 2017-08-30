cd435ddd57c117ff01ca498e3d69204e79c09fa3
mitrex4444 <44heroh/2mail.ru>
2017-08-30 23:10:21 +0300

    task14.task1405 - прошла с первой попытки

---
+++ src/task14/task1405/Solution.java
@@ -0,0 +1,41 @@
+package task14.task1405;
+
+/**
+ * Created by User on 30.08.2017.
+ */
+public class Solution {
+    public static void main(String[] args) {
+        Food food = new Food();
+        Selectable selectable = new Food();
+        Food newFood = (Food) selectable;
+
+        foodMethods(food);
+        selectableMethods(selectable);
+    }
+
+    public static void foodMethods(Food food) {
+        //тут добавьте вызов методов для переменной food
+        food.eat();
+        food.onSelect();
+    }
+
+    public static void selectableMethods(Selectable selectable) {
+        //тут добавьте вызов методов для переменной selectable
+        selectable.onSelect();
+    }
+
+    interface Selectable {
+        void onSelect();
+    }
+
+    static class Food implements Selectable {
+        public void eat() {
+            System.out.println("food was eaten");
+        }
+
+        @Override
+        public void onSelect() {
+            System.out.println("food was selected");
+        }
+    }
+}
