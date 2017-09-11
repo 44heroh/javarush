0477434bdb8c3f00b4bc86aa9ff57bec3f2b13cd
mitrex4444 <44heroh/2mail.ru>
2017-09-11 22:29:14 +0300

    task15.task1509 - прошла с 1 попытки

---
+++ src/task15/task1509/Solution.java
@@ -0,0 +1,30 @@
+package task15.task1509;
+
+/**
+ * Created by User on 11.09.2017.
+ */
+public class Solution {
+    public static void main(String[] args) {
+        Man man = new Man();
+        Woman woman = new Woman();
+
+        printName(man);
+        printName(woman);
+    }
+
+    public static void printName(Human human) {
+
+    }
+
+    public static class Human {
+
+    }
+
+    public static class Man extends Human {
+
+    }
+
+    public static class Woman extends Human {
+
+    }
+}
