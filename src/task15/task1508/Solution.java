6b55a6e3c172eb68868c00c563407e7ce7549943
mitrex4444 <44heroh/2mail.ru>
2017-09-11 22:27:29 +0300

    task15.task1508 - прошла с 1 попытки

---
+++ src/task15/task1508/Solution.java
@@ -0,0 +1,30 @@
+package task15.task1508;
+
+/**
+ * Created by User on 11.09.2017.
+ */
+public class Solution {
+    public static void main(String[] args) {
+        print(1);
+    }
+
+    /*public static void print(byte b) {
+        System.out.println("Я буду Java прогером1!");
+    }
+
+    public static void print(double d) {
+        System.out.println("Я буду Java прогером2!");
+    }*/
+
+    public static void print(long l) {
+        System.out.println("Я буду Java прогером3!");
+    }
+
+    /*public static void print(float f) {
+        System.out.println("Я буду Java прогером4!");
+    }
+
+    public static void print(char c) {
+        System.out.println("Я буду Java прогером5!");
+    }*/
+}
