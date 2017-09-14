c65bb0a9900526e1d712780614b9d53a182ad58f
mitrex4444 <44heroh/2mail.ru>
2017-09-14 19:35:49 +0300

    task15.task1516 - прошла с 5 попытки

---
+++ src/task15/task1516/Solution.java
@@ -0,0 +1,22 @@
+package task15.task1516;
+
+public class Solution {
+    public static int intVar;
+    public static double doubleVar;
+    public static Double DoubleVar;
+    public static boolean booleanVar;
+    public static Object ObjectVar;
+    public static Exception ExceptionVar;
+    public static String StringVar;
+
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        System.out.println(solution.intVar);
+        System.out.println(solution.doubleVar);
+        System.out.println(solution.DoubleVar);
+        System.out.println(solution.booleanVar);
+        System.out.println(solution.ObjectVar);
+        System.out.println(solution.ExceptionVar);
+        System.out.println(solution.StringVar);
+    }
+}
