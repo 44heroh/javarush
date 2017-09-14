e27a87c8b483210d787c504555340ebf1513847a
mitrex4444 <44heroh/2mail.ru>
2017-09-14 10:19:52 +0300

    task15.task1513 - прошла с 2 попытки

---
+++ src/task15/task1513/Solution.java
@@ -0,0 +1,19 @@
+package task15.task1513;
+
+/**
+ * Created by User on 14.09.2017.
+ */
+public class Solution {
+    public static void main(String[] args) {
+    }
+
+    public interface Runnable {
+
+    }
+
+    public class Machine implements Runnable {
+    }
+
+    public class Car extends Machine {
+    }
+}
