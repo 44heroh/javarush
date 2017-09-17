3b21b9e7e64225c6bade7cbdb5b2525111f12c45
mitrex4444 <44heroh/2mail.ru>
2017-09-17 22:31:24 +0300

    task15.task1523 - прошла с 2 раза

---
+++ src/task15/task1523/Solution.java
@@ -0,0 +1,20 @@
+package task15.task1523;
+
+public class Solution {
+    public static void main(String[] args) {
+
+    }
+
+    public Solution (){
+    }
+
+    protected Solution (int d){
+    }
+
+    private Solution (String d){
+    }
+
+    Solution (double d){
+
+    }
+}
---
+++ src/task15/task1523/SubSolution.java
@@ -0,0 +1,15 @@
+package task15.task1523;
+
+public class SubSolution extends Solution {
+
+    public SubSolution() {
+    }
+
+    protected SubSolution(int d) {
+
+    }
+
+    SubSolution (String f){
+
+    }
+}
